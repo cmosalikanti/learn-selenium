9cb0cf47acaf225565299eed4ce94f271731521e
Chandra Sekhar Mosalikanti <mcsekhar777@gmail.com>
2025-01-21 23:22:32 +0000

    added test for uploading a file using selenium driver.

---
+++ src/test/java/org/example/FileUpload.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class FileUpload {
+    public static void main(String[] args) {
+        new FileUpload().testFileUpload();
+    }
+    private void testFileUpload() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        URL fileUrl = getClass().getClassLoader().getResource("test.txt");
+
+        File file;
+        try {
+            file = new File(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String filePath = file.getAbsolutePath();
+        driver.findElement(By.name("my-file")).sendKeys(filePath);
+        driver.quit();
+    }
+}
